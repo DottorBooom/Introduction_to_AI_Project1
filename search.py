1a68eeb729cdbfea5a677a135f25105981640643
DottorBooom <94912692+DottorBooom@users.noreply.github.com>
2023-11-22 16:44:38 +0100

    V4.4.1: Search.py completed.
    As mentioned in the previous commit, codes and comments have been standardized across various algorithms. This is the final version of the search.py file, although some minor adjustments may be made during the completion of other tasks.

--- search.py
+++ search.py
@@ -74,6 +74,50 @@ def tinyMazeSearch(problem):
     print(lists)
     return  lists
 
+# Before looking at the code, I will make a summary of the data structure that we are gonna use and a summary of the concepts.
+
+###### Data structure ###### 
+
+# This part of the code is dedicated to declaring the data structures that will be used for DFS/BFS/UCS/A*.
+# In particular: a list to contain all the nodes that have been visited and a stack to insert and
+# the neighboring nodes that still need to be visited.
+# Additionally, we will need some way to keep track of the path for the solution, and for UCS/A* the cost of the path. 
+# This will be explained in more detail later in the code. 
+# For now, it's enough to know that these two structures will be the ones allowing us to navigate within the code.
+
+#visited = [] for saving the already visited noe
+#stack = util.Stack() used by DFS, a LIFO structure
+#queue = util.Queue() used by BFS, a FIFO structure
+#priorityqueue = util.PriorityQueue() used by UCS/A*, a FIFO structure with 
+
+###### Alforithm's behavior ###### 
+
+# Let's review briefly how the DFS algorithm behaves:
+#       1) Take a certain starting node and expand all neighboring nodes;
+#       2) Insert neighboring nodes into the stack, a Last In First Out (LIFO) structure;
+#       3) Mark the starting node as visited in the list of visited nodes;
+#       4) Perform a pop operation from the stack, and that node becomes the 'current_node';
+#       5) Repeat from step 1.
+
+# With that we wrote the general outline of the DFS algorithm. Let's now focus on the details:
+#
+# 1) Nodes are tuples of the form ((x, y), "Direction", Weight), where the first element indicates 
+# the coordinates of the node, the second indicates the direction in which Pacman should move to reach it, 
+# and the third indicates the weight to reach that specific position;
+# 2) DFS should return the path from the start node to the goal node. How do we follow 
+# the path without saving it in an additional structure? Every time we insert a new 
+# discovered node into the stack, we update the second element of the tuple from a string 
+# to a list containing all the movements necessary to reach that specific node.
+# 3) We need to keep track of the cost for reach each node. So, like we did with the path, we update the cost
+# before inserting them inside the priority queue. The priority queue will automatically rearange itself to put
+# first the lowest cost path
+# 4) Unlike BFS, if we find an already visited node while exploring some neighbor I don't skip it. Instead, I 
+# confront if the cost to reach that node is less than going throw this neighbor. If so, I remove that node from
+# the visited list, update the path and then insert it again in the priority queue.
+
+# And that is the summary of the algorithm we need to implement. Let's now analyze the code line by line.
+
+
 def depthFirstSearch(problem: SearchProblem):
     """
     Search the deepest nodes in the search tree first.
@@ -97,36 +141,10 @@ def depthFirstSearch(problem: SearchProblem):
 
     ###### Data structure ###### 
     
-    # This part of the code is dedicated to declaring the data structures that will be used for DFS.
-    # In particular: a list to contain all the nodes that have been visited and a stack to insert 
-    # the neighboring nodes that still need to be visited.
-    # Additionally, we will need some way to keep track of the path for the solution. 
-    # This will be explained in more detail later in the code. 
-    # For now, it's enough to know that these two structures will be the ones allowing us to navigate within the code.
-    
     visited = []
     stack = util.Stack()
 
     ###### DFS ###### 
-
-    # Let's review briefly how the DFS algorithm behaves:
-    #       1) Take a certain starting node and expand all neighboring nodes;
-    #       2) Insert neighboring nodes into the stack, a Last In First Out (LIFO) structure;
-    #       3) Mark the starting node as visited in the list of visited nodes;
-    #       4) Perform a pop operation from the stack, and that node becomes the 'current_node';
-    #       5) Repeat from step 1.
-
-    # With that we wrote the general outline of the DFS algorithm. Let's now focus on the details:
-    #
-    # 1) Nodes are tuples of the form ((x, y), "Direction", Weight), where the first element indicates 
-    # the coordinates of the node, the second indicates the direction in which Pacman should move to reach it, 
-    # and the third indicates the weight to reach that specific position;
-    # 2) DFS should return the path from the start node to the goal node. How do we follow 
-    # the path without saving it in an additional structure? Every time we insert a new 
-    # discovered node into the stack, we update the second element of the tuple from a string 
-    # to a list containing all the movements necessary to reach that specific node.
-
-    # And that is the summary of the algorithm we need to implement. Let's now analyze the code line by line.
 
     current_node = problem.getStartState() # Start with a given start node
     for i in problem.getSuccessors(current_node): # Expan the neighbors and go throw each one of them
@@ -163,37 +181,11 @@ def breadthFirstSearch(problem: SearchProblem):
     "*** YOUR CODE HERE ***"
     
     ###### Data structure ###### 
-    
-    # This part of the code is dedicated to declaring the data structures that will be used for BFS.
-    # In particular: a list to contain all the nodes that have been visited and a queue to insert 
-    # the neighboring nodes that still need to be visited.
-    # Additionally, we will need some way to keep track of the path for the solution. 
-    # This will be explained in more detail later in the code. 
-    # For now, it's enough to know that these two structures will be the ones allowing us to navigate within the code.
-    
+
     visited = []
     queue = util.Queue()
 
     ###### BFS ###### 
-
-    # Let's review briefly how the BFS algorithm behaves:
-    #       1) Take a certain starting node and expand all neighboring nodes;
-    #       2) Insert neighboring nodes into the queue, a Firs In First Out (FIFO) structure;
-    #       3) Mark the starting node as visited in the list of visited nodes;
-    #       4) Perform a pop operation from the queue, and that node becomes the 'current_node';
-    #       5) Repeat from step 1.
-
-    # With that we wrote the general outline of the BFS algorithm. Let's now focus on the details:
-    #
-    # 1) Nodes are tuples of the form ((x, y), "Direction", Weight), where the first element indicates 
-    # the coordinates of the node, the second indicates the direction in which Pacman should move to reach it, 
-    # and the third indicates the weight to reach that specific position;
-    # 2) BFS should return the path from the start node to the goal node. How do we follow 
-    # the path without saving it in an additional structure? Every time we insert a new 
-    # discovered node into the queue, we update the second element of the tuple from a string 
-    # to a list containing all the movements necessary to reach that specific node.
-
-    # And that is the summary of the algorithm we need to implement. Let's now analyze the code line by line.
 
     current_node = problem.getStartState() # Start with a given start node
     for i in problem.getSuccessors(current_node): # Expan the neighbors and go throw each one of them
@@ -235,67 +227,38 @@ def uniformCostSearch(problem: SearchProblem):
     "*** YOUR CODE HERE ***"
 
     ###### Data structure ###### 
-    
-    # This part of the code is dedicated to declaring the data structures that will be used for UCS.
-    # In particular: a list to contain all the nodes that have been visited and a priority queue to insert 
-    # the neighboring nodes that still need to be visited.
-    # Additionally, we will need some way to keep track of the path for the solution and the cost. 
-    # This will be explained in more detail later in the code. 
-    # For now, it's enough to know that these two structures will be the ones allowing us to navigate within the code.
-    
+
     visited = []
     priorityQueue = util.PriorityQueue()
 
     ###### UCS ###### 
 
-    # Let's review briefly how the  algorithm behaves:
-    #       1) Take a certain starting node and expand all neighboring nodes;
-    #       2) Insert neighboring nodes into the priority queue, particular type of structure that put the lowest path cost first;
-    #       3) Mark the starting node as visited in the list of visited nodes;
-    #       4) Perform a pop operation from the priority queue, and that node becomes the 'current_node';
-    #       5) Repeat from step 1.
+    # With UCS the code must change. We need to many differt thing, like keeping the cost and cheking if an already 
+    # visited node have a better path
 
-    # With that we wrote the general outline of the UCS algorithm. Let's now focus on the details:
-    #
-    # 1) Nodes are tuples of the form ((x, y), "Direction", Weight), where the first element indicates 
-    # the coordinates of the node, the second indicates the direction in which Pacman should move to reach it, 
-    # and the third indicates the weight to reach that specific position;
-    # 2) UCS should return the path from the start node to the goal node. How do we follow 
-    # the path without saving it in an additional structure? Every time we insert a new 
-    # discovered node into the priority queue, we update the second element of the tuple from a string 
-    # to a list containing all the movements necessary to reach that specific node.
-    # 3) We need to keep track of the cost for reach each node. So, like we did with the path, we update the cost
-    # before inserting them inside the priority queue. The priority queue will automatically rearange itself to put
-    # first the lowest cost path
-    # 4) Unlike BFS, if we find an already visited node while exploring some neighbor I don't skip it. Instead, I 
-    # confront if the cost to reach that node is less than going throw this neighbor. If so, I remove that node from
-    # the visited list, update the path and then insert it again in the priority queue.
+    current_node = (problem.getStartState(), [], 0.0)   # Start with a given start node. Why the unusual declaretion?
+                                                        # Because the nodes are composed like tuple of the form of
+                                                        # ((x,y),"Move"cost) but the start goal only have the coordinates.
+                                                        # Doing this I make sure that every node have the same form.
 
-    # And that is the summary of the algorithm we need to implement. Let's now analyze the code line by line.
+    if problem.isGoalState(current_node[0]): # Check if that node is a goal state
+        return [] # If the start node is the goal stat, return an empty path
+    
+    priorityQueue.push(current_node, 0.0) # Push the start node in the priority queue with cost equal to 0
+    visited.append(current_node) # And then insert it in the visited list
 
-    current_node = problem.getStartState() # Start with a given start node
-    visited.append(current_node)    # Insert only the coordinates of the start node in the visite list. 
-                                    # Why? Because we only need those for undertsand if a node is already be visited
-                                    # or if it's a goal state
+    while not priorityQueue.isEmpty(): # Unless the stack is empty, execute. Simple.
 
-    for i in problem.getSuccessors(current_node): # Expan the neighbors and go throw each one of them
-        i = list(i) # Change the neighbors from a Tuple to a List, that's cause the type Tuple would not allow us to 
-                    # change any type of his component
-        i[1] = [i[1]]   # Change the type of the direction from string to list, so we can insert the 
-                                # path to each node from the start 
-        visited.append(i) # Insert the visited neighbour in the visited list
-        priorityQueue.push(i,i[2])   # Push every neighbors in to the priority queue, the lowest cost one will be the next to be analyzed.
-
-    while not priorityQueue.isEmpty():  # Unless the priority queue is empty, execute. Simple.
-        current_node = priorityQueue.pop() # Pop the nex node from the priority queue 
-
+        current_node = priorityQueue.pop() # Pop the nex node from the stack
         if problem.isGoalState(current_node[0]): # If that particular node is the goal state
             return current_node[1] #Then, we can return the path that we save throw the execution of the code
 
-        neighbors = problem.getSuccessors(current_node[0]) # If it's not the goal, we expand its node        
-        for i in neighbors: # If the node is not a goal state we go throw every neighbors
+
+        for i in problem.getSuccessors(current_node[0]):    # If the current node is not a goal state
+                                                            # we expand all its neighbors and go throw every one
             i = list(i) # As already explained before the while, we change the type
-            if i[0] not in [x[0] for x in visited]: # we iterate inside the visited node and search for an already explored node
+            if i[0] not in [x[0] for x in visited]: # we iterate inside the visited list and search for an already explored node
+                
                 # The next one is foundamental
                 i[1] = current_node[1] + [i[1]] # If not, we set the path of the neighbour equal to the path from 
                                                 # start to the current + the path to reach the current neighbor
@@ -304,18 +267,18 @@ def uniformCostSearch(problem: SearchProblem):
                 visited.append(i)   # And then we insert that node in to the visited list, in this way
                                     # if the next node in the queue also have this neighbor is not gonna 
                                     # insert it in the queue again
-                priorityQueue.push(i, cost) # and than push it in with the updated cost
+                priorityQueue.push(i, cost) # and than push it in with the updated cost + the herustic cost
             else: # Otherwise, if the neighbor is in the vissited list
                 for j in visited: # We go throw each node in the visited list
-                    if i[0] == j[0] and j != problem.getStartState(): # Using the coordinates, if we find that particular node
-                                                                      # And it's not the starting node
+                    if i[0] == j[0]: # Using the coordinates, if we find that particular node
+                                    # And it's not the starting node
                         if j[2] > current_node[2] + i[2]:   # If the cost of that node is major of the current node + the cost to
                                                             # to reach the neighbor
                             visited.remove(j)   # Remove that particular node from the visited list
                             cost = current_node[2] + i[2]   # Update its cost with a new (and less) cost
                             j[2] = cost
                             j[1] = current_node[1] + [i[1]] # Update its new found path
-                            priorityQueue.push(j,cost) # And then reinstert it in the priority queue
+                            priorityQueue.push(j,cost) # And then reinstert it in the priority queue + the heuristic cost
 
     # And that's all for the UCS. Let's move on to the next one
 
@@ -351,9 +314,13 @@ def f(problem,state,heuristic):
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+
+    ###### Data structure ###### 
     
     visited = []
     priorityQueue = util.PriorityQueue()
+
+    ###### A* ######
 
     current_node = (problem.getStartState(), [], 0.0) # Start with a given start node
 
